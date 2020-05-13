ed6788b5b234dabdd6aaaa855bbab95d7bc05e92
claudio.nazareth <chtnazareth@gmail.com>
2020-05-13 21:37:31 +0200

    First commit

---
+++ src/main/kotlin/de/ynab/YnabApplication.kt
@@ -0,0 +1,11 @@
+package de.ynab
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class YnabApplication
+
+fun main(args: Array<String>) {
+  runApplication<YnabApplication>(*args)
+}
